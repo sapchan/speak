b0e9afe10217a0bf1e60f95507c175cd9caccba6
Sacheth Chandramouli <c.sacheth1@gmail.com>
2017-09-23 14:58:15 -0400

    yeaaaaaaa

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from flask import Flask, jsonify
 from flask import request,redirect,url_for
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 data = {}
 
+test = {}
 
 @app.route("/")
 def hello():
-    return "Hello Worlds!"
+    test['id'] = 'Hello World'
+    return jsonify({'test': test})
 
 @app.route("/submit_audio", methods=['POST'])
 def submit_audio():
     if request.method == 'POST':
-        uuid_value = request.args['uuid']
-        blob_value = request.args['blob']
+        json = request.get_json()
+        uuid_value = json.get('uuid')
+        #audio = request.files['file']
         data['uuid'] = str(uuid_value)
-        data['contextual'] = ['sup','yi']
+        #data['file_name'] = str(audio.filename)
         return redirect(url_for('get_audio'))
         #link_to_audio = request.files['file'].audio
 
